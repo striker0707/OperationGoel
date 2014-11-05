8eaf848968f10ed48058020e19ac3044a64cd7d8
Elton <eltonv93@gmail.com>
2014-11-05 12:21:57 -0800

    Update Node.h

--- Node.h
+++ Node.h
@@ -9,12 +9,12 @@ private:
    Node<ItemType>* next; 
    
 public:
-	Node()										{next = 0;}
-	Node(const ItemType& anItem)				{item = anItem; next = 0; }
+	Node()						{next = 0;}
+	Node(const ItemType& anItem)			{item = anItem; next = 0; }
 	void setItem(const ItemType& anItem)		{item = anItem;}
 	void setNext(Node<ItemType>* nextNodePtr)	{next = nextNodePtr;}
-	ItemType getItem() const					{return item;}
-	Node<ItemType>* getNext() const				{return next;}
+	ItemType getItem() const			{return item;}
+	Node<ItemType>* getNext() const			{return next;}
 }; 
 
 #endif
