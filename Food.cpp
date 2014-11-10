172149dc873336978c54c98e12fc8b8ad2b4a489
joannevtrinh <joannevtrinh@gmail.com>
2014-11-09 21:49:50 -0800

    Create Food.cpp

---
+++ Food.cpp
@@ -0,0 +1,102 @@
+#include "Food.h"
+
+Food::Food()
+{
+    name = "";
+    calories = 0;
+    gramsFat = 0;
+    cholesterol = 0;
+    sodium = 0;
+    protein = 0;
+}
+
+Food::Food(const Food &fobj)
+{
+    name = fobj.getName();
+    calories = fobj.getCalories();
+    gramsFat = fobj.getGramsFat();
+    cholesterol = fobj.getCholesterol();
+    sodium = fobj.getSodium();
+    protein = fobj.getProtein();
+    
+}
+
+Food::Food(string inputfname, float inputcal, float inputgFat, float inputcho, float inputsod, float inputpro)
+{
+    name = inputfname;
+    calories = inputcal;
+    gramsFat = inputgFat;
+    cholesterol = inputcho;
+    sodium = inputsod;
+    protein = inputpro;
+}
+
+string Food::getName() const
+{
+    return name;
+}
+
+float Food::getCalories() const
+{
+    return calories;
+}
+
+float Food::getGramsFat() const
+{
+    return gramsFat;
+}
+
+float Food::getCholesterol() const
+{
+    return cholesterol;
+}
+
+float Food::getSodium() const
+{
+    return sodium;
+}
+
+float Food::getProtein() const
+{
+    return protein;
+}
+
+void Food::setName(string inputname)
+{
+    name = inputname;
+}
+
+void Food::setCalories(float cal)
+{
+    calories = cal;
+}
+
+void Food::setGramsF(float gFat)
+{
+    gramsFat = gFat;
+}
+
+void Food::setCholesterol(float cholest)
+{
+    cholesterol = cholest;
+}
+
+void Food::setSodium(float inputsod)
+{
+    sodium = inputsod;
+}
+
+void Food::setProtein(float inputpro)
+{
+    protein = inputpro;
+}
+
+void Food::print(Food& fd)
+{
+    cout << "Name : " << fd.getName() << endl;
+    cout << "Calories : " << fd.getCalories() << endl;
+    cout << "Grams Fat : " << fd.getGramsFat() << endl;
+    cout << "Cholesterol : " << fd.getCholesterol() << endl;
+    cout << "Sodium : " << fd.getSodium() << endl;
+    cout << "Protein : " << fd.getProtein() << endl;
+}
