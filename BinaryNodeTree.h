b6007f88ae5223f25d4066fa391f5bfa14d76af7
locgiang <pyrocrust@gmail.com>
2014-11-14 13:08:02 -0800

    Update BinaryNodeTree.h

--- BinaryNodeTree.h
+++ BinaryNodeTree.h
@@ -91,9 +91,7 @@ BinaryNodeTree<ItemType>::BinaryNodeTree( const ItemType& rootItem)
 	rootPtr = new BinaryNode<ItemType>(rootItem, nullptr, nullptr );
 } // end constructor
 template < class ItemType>
-BinaryNodeTree<ItemType>::BinaryNodeTree( const ItemType& rootItem,
-											const BinaryNodeTree<ItemType>* leftTreePtr,
-													const BinaryNodeTree<ItemType>* rightTreePtr)
+BinaryNodeTree<ItemType>::BinaryNodeTree( const ItemType& rootItem, const BinaryNodeTree<ItemType>* leftTreePtr, const BinaryNodeTree<ItemType>* rightTreePtr)
 {
 	rootPtr = new BinaryNode<ItemType>(rootItem,
 	copyTree(leftTreePtr->rootPtr),
