7138e9f97a05803d49bbcca053f2fb4edfad6d8a
joannevtrinh <joannevtrinh@gmail.com>
2014-11-23 00:50:01 -0800

    Update BinarySearchTree.h

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -1,17 +1,17 @@
 #ifndef Lab3_BinarySearchTree_h
 #define Lab3_BinarySearchTree_h
-#include "Node.h"
+#include "BSTNode.h"
 #include "Queue.h"
 
 class BinarySearchTree
 {
     private:
 	//Declaration of variables
-    Node* root;
+    BSTNode* root;
 	//Private add, search, amd modify methods
-    bool addHelper(Node* n, Node* newNode);
-    bool searchHelper(Node* n, People pobj);
-    bool modifyHelper(Node* n, People pobj);
+    bool addHelper(BSTNode* n, BSTNode* newNode);
+    bool searchHelper(BSTNode* n, People pobj);
+    bool modifyHelper(BSTNode* n, People pobj);
     
     public:
 	//Default constructor
@@ -20,16 +20,16 @@ class BinarySearchTree
     bool search(People pobj);
     bool add(People peopleobj);
     bool remove(const People& pobj);
-    bool removeHelper(Node* n, const People& pobj);
-    Node* removeNode(Node* n);
+    bool removeHelper(BSTNode* n, const People& pobj);
+    Node* removeNode(BSTNode* n);
     
     void modify(People pobj, People newpobj);
     
 	//Ordering methods
     void inorder(ostream& stream);
-    void inorderHelper(Node* n, ostream& stream);
+    void inorderHelper(BSTNode* n, ostream& stream);
     void postorder(ostream& stream);
-    void postorderHelper(Node* n, ostream& stream);
+    void postorderHelper(BSTNode* n, ostream& stream);
     void breadthfirst(ostream& stream);
     
     void print();
