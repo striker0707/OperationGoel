1f04aaa6d3a504146ec46956aa26524167a3a923
joannevtrinh <joannevtrinh@gmail.com>
2014-11-23 00:51:43 -0800

    Update BSTNode.h

--- BSTNode.h
+++ BSTNode.h
@@ -8,28 +8,28 @@
 
 using namespace std;
 
-class Node
+class BSTNode
 {
 	//Declartion of variables
     private:
 		People personobj;
-		Node* leftptr;
-		Node* rightptr;
+		BSTNode* leftptr;
+		BSTNode* rightptr;
     
     public:
 		//Default constructor
-		Node();
-		Node(People inputperson);
+		BSTNode();
+		BSTNode(People inputperson);
 		
 		//Getter and setter functions
 		void setperson(People inputpobj);
 		People getperson();
     
-		Node* getleftptr();
-		Node* getrightptr();
+		BSTNode* getleftptr();
+		BSTNode* getrightptr();
     
-		void setleftptr(Node* inputlptr);
-		void setrightptr(Node* inputrptr);
+		void setleftptr(BSTNode* inputlptr);
+		void setrightptr(BSTNode* inputrptr);
 		//Print node attributes method
 		string print() const;
     
