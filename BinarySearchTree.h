9f36c1d1ca17de30bb52295ce37d50a76fdea1da
joannevtrinh <joannevtrinh@gmail.com>
2014-11-23 01:23:17 -0800

    Update BinarySearchTree.h

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -10,20 +10,20 @@ class BinarySearchTree
     BSTNode* root;
 	//Private add, search, amd modify methods
     bool addHelper(BSTNode* n, BSTNode* newNode);
-    bool searchHelper(BSTNode* n, People pobj);
-    bool modifyHelper(BSTNode* n, People pobj);
+    bool searchHelper(BSTNode* n, Food fobj);
+    bool modifyHelper(BSTNode* n, Food fobj);
     
     public:
 	//Default constructor
     BinarySearchTree();
 	//Search, modify, add, and remove methods
-    bool search(People pobj);
-    bool add(People peopleobj);
-    bool remove(const People& pobj);
-    bool removeHelper(BSTNode* n, const People& pobj);
+    bool search(Food fobj);
+    bool add(Food foodobj);
+    bool remove(const Food& fobj);
+    bool removeHelper(BSTNode* n, const Food& fobj);
     Node* removeNode(BSTNode* n);
     
-    void modify(People pobj, People newpobj);
+    void modify(Food fobj, Food newfobj);
     
 	//Ordering methods
     void inorder(ostream& stream);
