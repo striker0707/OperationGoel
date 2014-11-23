2096a4962abf009d82f48e54d9bee9a3f45ae386
Elton <eltonv93@gmail.com>
2014-11-23 13:08:28 -0800

    Moved menu() from main to FoodProgram

--- FoodProgram.h
+++ FoodProgram.h
@@ -5,20 +5,21 @@
 
 using namespace std;
 
-
 class FoodProgram
 {
 private:
 	SortedList<Food> fDB;
 	HashTable<string, Food*> fHT;
+	string fileName;
 public:
 	FoodProgram();
 	/** Parses food file and inputs into fDB */
 	FoodProgram(string filename);
 	/** Saves any changes made to fDB to file */
 	~FoodProgram();
-	bool fileIO(string filename);
+	bool fileIO();
 	void menu() const;
+	/** Accepts option and builds BST from fDB sorted by said option */
 	bool buildBST(int option);
 	/** Inserts new entries into fDB */
 	bool insert(string newEntry);
@@ -28,3 +29,53 @@ public:
 };
 
 /* Remember that all food data needs to be stored as dynamic memory in both the BST and HT */
+
+void FoodProgram::menu() const
+{
+	char choice = -1;
+ 
+	while(choice != 9)
+	{
+		//menu text is taken directly from the project req
+		//recommend we change or lsightly modify some of the names so more "user friendly"ish
+		cout << "     MENU" << endl;
+		cout << "1) Add new data" << endl;
+		cout << "2) Delete data" << endl;
+		cout << "3) Find and display one data record using the primary key" << endl; 
+		cout << "4) List data in hash table sequence" << endl;
+		cout << "5) List data in key sequence (sorted)" << endl;
+		cout << "6) Print indented tree" << endl;
+		cout << "7) Effeciency" << endl;
+		cout << "8) SECRET MENU OPTION" << endl;
+		cout << "9) Quit" << endl;
+    
+		cin >> choice;
+    
+		//ENTER FUNCTION CALLS FOR EACH MODULE AS WE COMPLETE IT,
+		//PLEASE COMMENT OUT YOUR FUNCTION CALL IF YOU COMMIT A CHANGE THAT IS NOT COMPILABLE
+		//switch(choice)
+		//case 1:	
+		//	break;
+		//case 2:
+		//  break;
+		//case 3:
+		//  break;
+		//case 4:
+		//  break;
+		//case 5:
+		//  break;
+		//case 6:
+		//  break;
+		//case 7:
+		//  break;
+		//case 8:
+		//  break;
+		//case 9: //user quit case
+		//  return 0;
+		//  break;
+		//default:
+		//cout << "Invalid Option, please enter a valid option (1-9)." << endl;
+		//system("pause");
+		//break;
+	}
+}
--- main.cpp
+++ main.cpp
@@ -15,7 +15,6 @@ using namespace std;
 //void writetoOutput(ofstream &stream, HashTable<string,Food> hobj);
 
 //void printToOutput(ofstream &outputFile, Hashtable hashtableobj); // Need hashtable obj that we will print out our memory to outputfile
-void menu();
 
 int main()
 {
@@ -44,10 +43,6 @@ int main()
 		Food newFood = Food(fname, cal, gfat, chol, sodi, prot);
 		//hashedTable.insert
 	}
-  
-  
-  
-    menu();
     
     //writetoOutput(outputFile, hashedTable);
 	
@@ -79,55 +74,6 @@ int main()
     return 0;
 }
 
-void menu()
-{
-  char choice = -1;
- 
-  while(choice != 9)
-  {
-    //menu text is taken directly from the project req
-    //recommend we change or lsightly modify some of the names so more "user friendly"ish
-    cout << "     MENU" << endl;
-    cout << "1) Add new data" << endl;
-    cout << "2) Delete data" << endl;
-    cout << "3) Find and display one data record using the primary key" << endl; 
-    cout << "4) List data in hash table sequence" << endl;
-    cout << "5) List data in key sequence (sorted)" << endl;
-    cout << "6) Print indented tree" << endl;
-    cout << "7) Effeciency" << endl;
-    cout << "8) SECRET MENU OPTION" << endl;
-    cout << "9) Quit" << endl;
-    
-    cin >> choice;
-    
-    //ENTER FUNCTION CALLS FOR EACH MODULE AS WE COMPLETE IT,
-    //PLEASE COMMENT OUT YOUR FUNCTION CALL IF YOU COMMIT A CHANGE THAT IS NOT COMPILABLE
-    //switch(choice)
-    //case 1:
-    //  break;
-    //case 2:
-    //  break;
-    //case 3:
-    //  break;
-    //case 4:
-    //  break;
-    //case 5:
-    //  break;
-    //case 6:
-    //  break;
-    //case 7:
-    //  break;
-    //case 8:
-    //  break;
-    //case 9: //user quit case
-    //  return 0;
-    //  break;
-    //default:
-    //cout << "Invalid Option, please enter a valid option (1-9)." << endl;
-    //system("pause");
-    //break;
-  }
-}
 
 bool openinputFile (ifstream &inputFile)
 {
